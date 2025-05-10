d315128b75d17f4bfa557e0ce7246da7007211fb
dhwisema <dhwisema@ncsu.edu>
2025-05-10 03:04:43 -0400

    a

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -10,8 +10,8 @@
         plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
         hash = "sha256-USKNTAvxmuxzhqA8e8XERr1U8513ONG54Md5vcDUERg=";
       };
-      virtualHosts."https://tail8f3db1.ts.net".extraConfig = ''
-        bind tailscale/calibre
+      virtualHosts."https://calibre-web.tail8f3db1.ts.net".extraConfig = ''
+        bind tailscale/calibre-web
         reverse_proxy localhost:8083
       '';
     };
