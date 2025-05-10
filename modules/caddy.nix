0d889290427d6231662426e3411abddb4f22bea2
dhwisema <dhwisema@ncsu.edu>
2025-05-10 02:04:31 -0400

    caddy.nix

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -2,10 +2,6 @@
   pkgs,
   ...
 }:
-let # Change these values
-  domain_name = "aaa.aaa.ts.net";
-  secure_token = "test";
-in
 {
   services = {
     caddy = {
@@ -14,24 +10,10 @@ in
         plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
         hash = "sha256-UR9CG/zIslkXHDj1fDWmhx8hJZ8VLvZzOTGvGqqx1Ls=";
       };
-      virtualHosts."https://matrix.aaa.ts.net".extraConfig = ''
-        bind tailscale/matrix
-        reverse_proxy localhost:6167
+      virtualHosts."https://tail8f3db1.ts.net".extraConfig = ''
+        bind tailscale/calibre
+        reverse_proxy localhost:8083
       '';
-    };
-    matrix-conduit = {
-      enable = true;
-      settings.global = {
-        # address = "127.0.0.1"; 
-        address = "0.0.0.0";
-        # Change this to `false` after the first user (admin) is registered,
-        # and then run `$ nixos-rebuild switch`.
-        allow_registration = true;
-        registration_token = "${secure_token}";
-        database_backend = "rocksdb";
-        port = 6167;
-        server_name = "${domain_name}";
-      };
     };
   };
 }
