4626955d9cfd5a8ee872ca5df177aeff8c035452
dhwisema <dhwisema@ncsu.edu>
2025-05-10 02:34:29 -0400

    hash

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -8,7 +8,7 @@
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
-        hash = "sha256-UR9CG/zIslkXHDj1fDWmhx8hJZ8VLvZzOTGvGqqx1Ls=";
+        hash = "sha256-USKNTAvxmuxzhqA8e8XERr1U8513ONG54Md5vcDUERg=";
       };
       virtualHosts."https://tail8f3db1.ts.net".extraConfig = ''
         bind tailscale/calibre
