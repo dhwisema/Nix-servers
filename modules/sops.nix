1781bd69dcb0c699d855795b0a44ca44dd72daea
dhwisema <dhwisema@ncsu.edu>
2025-05-10 02:08:45 -0400

    typo

--- modules/sops.nix
+++ modules/sops.nix
@@ -7,7 +7,7 @@ in
 {
   sops = {
     age.sshKeyPaths = map getKeyPath keys;
-    secrets.tailscale_key = { sopsFile =. /../host/secrets.yaml;};
+    secrets.tailscale_key = { sopsFile =./../host/secrets.yaml;};
 
   };
 }
