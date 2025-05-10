7a7c70e282dcfa86036596f9a10c7e05bff6a127
dhwisema <dhwisema@ncsu.edu>
2025-05-10 02:23:01 -0400

    test

--- host/configuration.nix
+++ host/configuration.nix
@@ -31,7 +31,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   users = {
-    mutableUsers = false;
+    mutableUsers = true;
     users.${"howard"} = {
       isNormalUser = true;
       extraGroups = ["networkmanager" "wheel"];
