534b4be1b1ff8b750e7df083431e808bcddca49a
dhwisema <dhwisema@ncsu.edu>
2025-05-10 02:43:19 -0400

    a

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, ... }:
 {
+
+services.tailscale.permitCertUid ="caddy";	
   services.tailscale.enable = true;
   services.tailscale.authKeyFile = config.sops.secrets.tailscale_key.path;
   }
