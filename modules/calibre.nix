08e2238a60a52e60b3521d7f1a6ff831ce04da08
dhwisema <dhwisema@ncsu.edu>
2025-07-12 03:11:12 -0400

    adding missing pacakges

--- modules/calibre.nix
+++ modules/calibre.nix
@@ -2,7 +2,29 @@
   # Calibre-Web
   services.calibre-web= {
     package = pkgs.calibre-web.overrideAttrs ({ propagatedBuildInputs ? [ ], ... }: {
-    propagatedBuildInputs = propagatedBuildInputs ++ [pkgs.python312Packages.packaging pkgs.python312Packages.jsonschema pkgs.python312Packages.setuptools ];});
+    propagatedBuildInputs = propagatedBuildInputs ++ [
+    pkgs.python312Packages.packaging 
+    pkgs.python312Packages.jsonschema 
+    pkgs.python312Packages.setuptools
+    pkgs.python312Packages.babel 
+    pkgs.python312Packages.flask
+    pkgs.python312Packages.pycountry
+    pkgs.python312Packages.apscheduler
+    pkgs.python312Packages.pypdf    
+    pkgs.python313Packages.pytz
+    pkgs.python312Packages.requests
+    pkgs.python312Packages.sqlalchemy
+    pkgs.python312Packages.tornado 
+    pkgs.python312Packages.wand  
+    pkgs.python312Packages.unidecode  
+    pkgs.python312Packages.lxml  
+    pkgs.python312Packages.chardet  
+    pkgs.python312Packages.netifaces-plus  
+    pkgs.python312Packages.urllib3  
+    pkgs.python312Packages.regex  
+    pkgs.python312Packages.bleach  
+    pkgs.python312Packages.python-magic  
+    pkgs.python312Packages.cryptography  ];});
     enable = true;
     group = "media";
     listen = {
