fa833a940229e0694aeddf1875d85cdf66f51c59
dhwisema <dhwisema@ncsu.edu>
2025-07-12 02:43:15 -0400

    fix build issue?

--- modules/calibre.nix
+++ modules/calibre.nix
@@ -2,7 +2,7 @@
   # Calibre-Web
   services.calibre-web= {
     package = pkgs.calibre-web.overrideAttrs ({ propagatedBuildInputs ? [ ], ... }: {
-    propagatedBuildInputs = propagatedBuildInputs ++ [ pkgs.python312Packages.jsonschema ];});
+    propagatedBuildInputs = propagatedBuildInputs ++ [ pkgs.python312Packages.jsonschema python312Packages.setuptools ];});
     enable = true;
     group = "media";
     listen = {
