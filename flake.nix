562a648c695bdafb4859e5d34f2d2155570147a7
dhwisema <dhwisema@ncsu.edu>
2025-10-12 03:53:53 -0400

    test unstable

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Flake for my oci linux servbor";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko/latest";
     disko.inputs.nixpkgs.follows = "nixpkgs";
    
