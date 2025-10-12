e2b8e2c16bbcc19bbb1f171decdd124a16f16af8
dhwisema <dhwisema@ncsu.edu>
2025-10-12 04:09:31 -0400

    maybe mae it cleaner

--- modules/calibre.nix
+++ modules/calibre.nix
@@ -1,36 +1,10 @@
 { config, lib, pkgs, ... }:{
   # Calibre-Web
   services.calibre-web= {
-    package = pkgs.calibre-web.overrideAttrs ({ propagatedBuildInputs ? [ ], ... }: {
-    propagatedBuildInputs = propagatedBuildInputs ++ [
-    pkgs.python312Packages.packaging 
-    pkgs.python312Packages.jsonschema 
-    pkgs.python312Packages.setuptools
-    pkgs.python312Packages.babel 
-    pkgs.python312Packages.flask
-    pkgs.python312Packages.pycountry
-    pkgs.python312Packages.apscheduler
-    pkgs.python312Packages.pypdf    
-    pkgs.python312Packages.pytz
-    pkgs.python312Packages.requests
-    pkgs.python312Packages.sqlalchemy
-    pkgs.python312Packages.tornado 
-    pkgs.python312Packages.wand  
-    pkgs.python312Packages.unidecode  
-    pkgs.python312Packages.lxml  
-    pkgs.python312Packages.chardet  
-    pkgs.python312Packages.netifaces-plus  
-    pkgs.python312Packages.urllib3  
-    pkgs.python312Packages.regex  
-    pkgs.python312Packages.bleach  
-    pkgs.python312Packages.python-magic  
-    pkgs.python312Packages.cryptography
-    pkgs.python312Packages.installer
-    pkgs.python312Packages.flask-principal
-    pkgs.python312Packages.flask-babel
-    pkgs.python312Packages.cffi
-    
-      ];});
+    package = pkgs.calibre-web.overridePythonAttrs (old: rec {
+      dependencies = old.dependencies ++ old.optional-dependencies.kobo;
+    });
+
     enable = true;
     group = "media";
     listen = {
