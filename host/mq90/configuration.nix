fc0f71a283b607a6de40d9ddc2a5bf3d602bf754
dhwisema <dhwisema@ncsu.edu>
2025-10-13 23:48:40 -0400

    the modules werent good

--- host/mq90/configuration.nix
+++ host/mq90/configuration.nix
@@ -7,12 +7,12 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ./../modules/common/common.nix
-    ./../modules/services/services.nix
+    ./../../modules/common/common.nix
+    ./../../modules/services/services.nix
   ];
 
   networking.hostName = "MQ90";
-  Cweb.enable = true;
+  Cweb.enable = false;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
