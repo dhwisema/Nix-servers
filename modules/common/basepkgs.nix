a2884ec6882b99cf912de67e79d363a3667588aa
dhwisema <dhwisema@ncsu.edu>
2025-10-13 23:12:52 -0400

    formatting

--- modules/common/basepkgs.nix
+++ modules/common/basepkgs.nix
@@ -1,17 +1,21 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
 
-
   environment.systemPackages = with pkgs; [
-     curl
-     git
-     wget
-     lazygit
-     yazi
-     pik
-     btop
-     sqlite
+    curl
+    git
+    wget
+    lazygit
+    yazi
+    pik
+    btop
+    sqlite
   ];
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
-  }
+}
