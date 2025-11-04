0db7c283e0f766faab0c44affbcfc71bb1ea3f2f
dhwisema <dhwisema@ncsu.edu>
2025-11-03 22:24:58 -0500

    didnt like that one

--- modules/services/docker-compose.nix
+++ modules/services/docker-compose.nix
@@ -1,23 +1,6 @@
+
 # Auto-generated using compose2nix v0.3.1.
 { pkgs, lib, ... }:{
-  # Runtime
-  virtualisation.podman = {
-    enable = true;
-    autoPrune.enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings = {
-      # Required for container networking to be able to use names.
-      dns_enabled = true;
-    };
-  };
-
-  # Enable container name DNS for non-default Podman networks.
-  # https://github.com/NixOS/nixpkgs/issues/226365
-  networking.firewall.interfaces."podman+".allowedUDPPorts = [ 53 ];
-# Auto-generated using compose2nix v0.3.1.
-{ pkgs, lib, ... }:
-
-{
   # Runtime
   virtualisation.podman = {
     enable = true;
