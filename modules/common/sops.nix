fe07fe92fd1802ebb2a36026001eb88d053b91c0
dhwisema <dhwisema@ncsu.edu>
2025-11-04 04:18:10 -0500

    try again

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -11,13 +11,15 @@
   sops.age.generateKey = true;
   # This is the actual specification of the secrets.
 
-  # sops.secrets."Booklore/DATABASE_URL" = {};
-  # sops.secrets."Booklore/DB_USER" = {};
-  # sops.secrets."Booklore/DB_PASSWORD" = {};
-  # sops.secrets."mariaDB/MYSQL_ROOT_PASSWORD" = {};
-  # sops.secrets."mariaDB/MYSQL_DATABASE" = {};
-  sops.secrets."Booklore" = {};
-  sops.secrets."mariaDB" = {};
+
+
+  sops.secrets."Booklore/DATABASE_URL" = {};
+  sops.secrets."Booklore/DB_USER" = {};
+  sops.secrets."Booklore/DB_PASSWORD" = {};
+  sops.secrets."mariaDB/MYSQL_ROOT_PASSWORD" = {};
+  sops.secrets."mariaDB/MYSQL_DATABASE" = {};
+
+
 
 
 }
