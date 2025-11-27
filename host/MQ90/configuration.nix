620f2213320ccbe6c35d525c520eac9c9e7f87f1
dhwisema <dhwisema@ncsu.edu>
2025-11-27 01:14:18 -0500

    fix config mq90

--- host/MQ90/configuration.nix
+++ host/MQ90/configuration.nix
@@ -12,7 +12,6 @@
   ];
 
   networking.hostName = "MQ90";
-  Cweb.enable = false;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
